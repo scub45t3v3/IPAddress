e660e9de8e51cf68c8578b0b2b6704659fba9d98
Steve Winberg <stevewin21@gmail.com>
2018-08-18 15:48:43 +0700

    refactor test/index for readability

--- test/index.js
+++ test/index.js
@@ -1,35 +1,51 @@
-(function() {
-  var IP, IP4Address, IP6Address, unit;
+'use strict';
 
-  unit = require('unit.js');
+(() => {
+  // require dependencies
+  const unit = require('unit.js');
+  const {
+    IP,
+    IP4Address,
+    IP6Address,
+  } = require('../index');
 
-  ({IP, IP4Address, IP6Address} = require('../index'));
+  // describe IP
+  describe('IP', () => {
+    it('should be a function', () => {
+      unit
+        .function(IP);
+    }); // end it
 
-  describe('IP', function() {
-    it('should be a function', function() {
-      unit.function(IP);
-      return null;
-    });
-    it('should return an instance of IP4Address when given a version 4 ip string', function() {
-      unit.object(IP('127.0.0.1')).isInstanceOf(IP4Address).object(IP('192.168.0.1')).isInstanceOf(IP4Address);
-      return null;
-    });
-    it('should return an instance of IP6Address when given a version 6 ip string', function() {
-      unit.object(IP('::1')).isInstanceOf(IP6Address).object(IP('2001:db8::ff00:42:8329')).isInstanceOf(IP6Address);
-      return null;
-    });
-    return it('should throw an Error when given an invalid ip address', function() {
-      unit.error(function() {
-        return IP('a');
-      }).error(function() {
-        return IP(5);
-      }).error(function() {
-        return IP([]);
-      }).error(function() {
-        return IP({});
-      });
-      return null;
-    });
-  });
+    it('should return an instance of IP4Address when given a version 4 ip string', () => {
+      unit
+        .object(IP('127.0.0.1'))
+        .isInstanceOf(IP4Address)
+        .object(IP('192.168.0.1'))
+        .isInstanceOf(IP4Address);
+    }); // end it
 
-}).call(this);
+    it('should return an instance of IP6Address when given a version 6 ip string', () => {
+      unit
+        .object(IP('::1'))
+        .isInstanceOf(IP6Address)
+        .object(IP('2001:db8::ff00:42:8329'))
+        .isInstanceOf(IP6Address);
+    }); // end it
+
+    it('should throw an Error when given an invalid ip address', () => {
+      unit
+        .error(() => {
+          IP('a');
+        })
+        .error(() => {
+          IP(5);
+        })
+        .error(() => {
+          IP([]);
+        })
+        .error(() => {
+          IP({});
+        });
+    }); // end it
+  }); // end describe IP
+})(); // end IIFE
